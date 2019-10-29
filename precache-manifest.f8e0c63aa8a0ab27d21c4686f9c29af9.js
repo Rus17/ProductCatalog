2072c0e05d5a91640275a1fd48e22256dfe18c55
Nazarenko Ruslan <rus.nazarenko@gmail.com>
2019-10-29 08:02:04 +0200

    Updates

---
+++ precache-manifest.f8e0c63aa8a0ab27d21c4686f9c29af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497b8c0599112a08679071d3051ed4a7",
+    "url": "/ProductCatalog/index.html"
+  },
+  {
+    "revision": "eb566f6b70a475ecb32a",
+    "url": "/ProductCatalog/static/css/main.36899264.chunk.css"
+  },
+  {
+    "revision": "fcfcf8f7efda5924e1f2",
+    "url": "/ProductCatalog/static/js/2.0d6e8adf.chunk.js"
+  },
+  {
+    "revision": "eb566f6b70a475ecb32a",
+    "url": "/ProductCatalog/static/js/main.5e8eb804.chunk.js"
+  },
+  {
+    "revision": "abb48336b61c19894362",
+    "url": "/ProductCatalog/static/js/runtime-main.4ad54ec0.js"
+  },
+  {
+    "revision": "a66ca6b9a98b268c8ea74939c0192537",
+    "url": "/ProductCatalog/static/media/spinner.a66ca6b9.svg"
+  }
+]);
